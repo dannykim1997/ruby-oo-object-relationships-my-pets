e91cbc8204cd7a7f5bc7b82e19e0691c2c45efd8
github username <github email address>
2021-04-08 16:23:01 -0700

    commit tree a907e8ad047c68d288b32cda0d8adc61a77413bc

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name, species="human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.filter{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.filter{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.map{|dog| dog.mood="happy"}
+  end
+
+  def feed_cats
+    self.cats.map{|cat| cat.mood="happy"}
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+
+    pets.each{|pet| 
+      pet.mood="nervous"
+      pet.owner=nil
+    }
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
